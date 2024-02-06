7fe7d38ed81daf134da9caffb91a0aa16e2b042f
sanimations <smartucci157@gmail.com>
2024-02-06 17:31:45 -0500

    Updated the color palette and added more detail to Other Works section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Image } from "react-bootstrap";
 import "./App.css";
 
 function App() {
@@ -6,116 +7,134 @@ function App() {
     <div className="App">
       {/* Header */}
       <header>
-        <h1>Samuel Martucci</h1>
+        <h1 style={{ color: '#00FFFF'}} >Samuel Martucci</h1>
       </header>
 
       {/* Main Content */}
       <main>
         <section id="projects">
-          <h2>Projects</h2>
+          <h2 style={{ color: '#FF69B4'}}>Projects</h2>
 
           <div className="projects-container">
       
             <div className="project">
-              <h3>MyFlix React</h3>
-              <p>
-                Created a Meet App to find interesting events around the world.
-              </p>
+              <h3 style={{ color: '#FFA500'}}>MyFlix React</h3>
+              <p></p>
               <a
                 href="https://github.com/sanimations/myFlix-client"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <img
+                <Image
                   src={require("./assets/myFlix-React.png")}
                   alt="Movie Database React"
+                  rounded
                 />
               </a>
+              <p></p>
+              <p>
+                Created a Meet App to find interesting events around the world.
+              </p>
             </div>
 
             <div className="project">
-              <h3>MyFlix Angular</h3>
-              <p>
-                My queer movie database created with Angular.
-              </p>
+              <h3 style={{ color: '#FFA500'}}>MyFlix Angular</h3>
+              <p></p>
               <a
                 href="https://github.com/sanimations/myFlixAngularClient"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <img
+                <Image
                   src={require("./assets/myFlix-Angular.png")}
                   alt="Movie Database Angular"
+                  rounded
                 />
               </a>
+              <p></p>
+              <p>
+                My queer movie database created with Angular.
+              </p>
             </div>
 
             <div className="project">
-              <h3>WalkinTalkin</h3>
-              <p>
-                This is a chat app I created with React Native!  A mobile native app with chat, image, and location sharing.
-              </p>
+              <h3 style={{ color: '#FFA500'}}>WalkinTalkin</h3>
+              <p></p>
               <a
                 href="https://github.com/sanimations/WalkinTalkin"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <img
+                <Image
                   src={require("./assets/chatApp-reactNative.png")}
                   alt="Chat app"
+                  rounded
                 />
               </a>
+              <p></p>
+              <p>
+                This is a chat app I created with React Native!  A mobile native app with chat, image, and location sharing.
+              </p>
             </div>
 
             <div className="project">
-              <h3>Meet App</h3>
-              <p>
-                Created a Meet App to find interesting events around the world.
-              </p>
+              <h3 style={{ color: '#FFA500'}} >Meet App</h3>
+              <p></p>
               <a
                 href="https://sanimations.github.io/meet/"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <img
+                <Image
                   src={require("./assets/MeetApp-js.png")}
                   alt="Meet App"
+                  rounded
                 />
               </a>
+              <p></p>
+              <p>
+                Created a Meet App to find interesting events around the world.
+              </p>
             </div>
 
             <div className="project">
-              <h3>HTML Portfolio</h3>
-              <p>
-                This was the first ever Portfolio I made using HTML, CSS, and
-                JS. I also made working buttons and links that lead to other
-                pages
-              </p>
+              <h3 style={{ color: '#FFA500'}} >HTML Portfolio</h3>
+              <p></p>
               <a
                 href="https://sanimations.github.io/portfolio-website/index.html"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <img
+                <Image
                   src={require("./assets/portfolio-html.png")}
                   alt="Portfolio HTML"
+                  rounded
                 />
               </a>
+              <p></p>
+              <p>
+                This was the first ever Portfolio I made using HTML, CSS, and
+                JS. I also made working buttons and links that lead to other
+                pages
+              </p>
             </div>
 
             <div className="project">
-              <h3>PokeDex</h3>
-              <p>Created a personal pokedex linking to the pokemon API.</p>
+              <h3 style={{ color: '#FFA500'}}>PokeDex</h3>
+              <p></p>
               <a
                 href="https://sanimations.github.io/pokedex-js/"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <img
+                <Image
                   src={require("./assets/pokedex-js.png")}
                   alt="PokeDex Website"
+                  rounded
                 />
               </a>
+              <p></p>
+              <p>Created a personal pokedex linking to the pokemon API.</p>
             </div>
           </div>
         </section>
@@ -123,11 +142,11 @@ function App() {
 
       {/* Other Work Section */}
       <section id="other-work">
-        <h2>Other Work</h2>
+        <h2 style={{ color: '#FF69B4'}} >Other Work</h2>
         <p>
-          Here you can showcase any additional work you've done, such as
-          freelance projects, personal projects, or contributions to open-source
-          projects.
+          I worked as a Freelance Animator for Edgeworx Studios, most notably on the first season of the Hulu Show called "Wild Crime".
+          <br></br>I also worked on a music video for the band NephewCube on Spotify.  Here is the <a href="https://www.youtube.com/watch?v=oIYrEnYwzGk"
+                target="_blank" rel="noopener noreferrer">link</a>
         </p>
       </section>
 
