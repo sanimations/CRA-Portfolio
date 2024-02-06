062df19e029bb11e2bb922c40979f1b279fc9379
sanimations <smartucci157@gmail.com>
2024-02-06 14:42:22 -0500

    Updated index.jsx file

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Finds the root of your app
+const container = document.querySelector('#root');
+const root = createRoot(container);
+
 root.render(
   <Container>
     <App />
   </Container>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
