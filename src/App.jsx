18d28a39075d58c18a369c259c2dc57eba9e9083
sanimations <smartucci157@gmail.com>
2024-02-13 15:08:18 -0500

    Updated Portfolio webpage

--- src/App.jsx
+++ src/App.jsx
@@ -18,14 +18,24 @@ function App() {
         {/* Education */}
         <section id="education">
           <h2 style={{ color: "#66CDAA" }}>Education</h2>
-          <p>
-            Completed CareerFoundry's Intro To Frontend course as well as the
-            Full-Stack Immersion course.<br></br>
-            Recieved a Degree in Film/Electronic Arts at Bard College.<br></br>I
-            have now learned and used HTML, CSS, JavaScript, React.js, React
-            Native, and Angular.<br></br>
-            Blender, 3dsMax, Adobe Premiere Pro, Adobe After Effects, Adobe
-            Audtion, Adobe Animate
+          <p className="education-p">
+            I have recieved a Bachelor's Degree in Film/Electronic Arts at Bard
+            College.<br></br>I completed CareerFoundry's Intro To Frontend
+            course as well as the Full-Stack Immersion course.{" "}
+            <a
+              href="https://www.linkedin.com/posts/samuel-martucci-57710317b_im-happy-to-share-that-ive-completed-the-activity-7090117281665441792-Dsz2?utm_source=share&utm_medium=member_desktop"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Intro Certificate
+            </a>
+            <br></br>
+            Skills I learned at Bard College: Blender, 3dsMax, Adobe Premiere
+            Pro, Adobe After Effects, Adobe Audition, Adobe Animate, Adobe
+            Photoshop<br></br>
+            Skills I learned at CareerFoundry: HTML, CSS, JavaScript, React.js,
+            React Native, SQL, noSQL, Mongo, MongoDB, MongoDB Atlas,
+            accessibility (ARIA), React Bootstrap, JQuery and Angular.
           </p>
         </section>
 
@@ -51,7 +61,28 @@ function App() {
               </a>
               <p></p>
               <p>
-                Created a Meet App to find interesting events around the world. 
+                Using React, I built the client-side for an app called myFlix
+                based on its existing server-side code. The server-side code
+                came from a REST API and database I had created. In this project
+                I used the MERN tech stack and I learned how to use
+                Authentication with the accompanying API.
+                <br></br>
+                <a
+                  href="https://qfdb.netlify.app/login"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  MyFlix React Webpage
+                </a>
+                <br></br>
+                <a
+                  href="https://github.com/sanimations/myFlix-client"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {" "}
+                  MyFlix React Github{" "}
+                </a>
               </p>
             </div>
 
@@ -73,9 +104,27 @@ function App() {
               </a>
               <p></p>
               <p>
-                This was the first ever Portfolio I made using HTML, CSS, and
-                JS. I also made working buttons and links that lead to other
-                pages
+                This portfolio was my first coding assignment! I built a
+                responsive portfolio website using HTML and CSS. I used a grid
+                layout and coded an animation with an SVG image file. Finally, I
+                uploaded this site to my github pages.
+                <br></br>
+                <a
+                  href="https://sanimations.github.io/portfolio-website/index.html"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  HTML Portfolio Webpage
+                </a>
+                <br></br>
+                <a
+                  href="https://github.com/sanimations/portfolio-website/tree/main"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {" "}
+                  HTML Portfolio Github{" "}
+                </a>
               </p>
             </div>
 
@@ -96,7 +145,30 @@ function App() {
                 />
               </a>
               <p></p>
-              <p>Created a personal pokedex linking to the pokemon API.</p>
+              <p>
+                The objective of this project was to build a small web
+                application with HTML, CSS, and JavaScript that loads data from
+                an external API and enables the viewing of data points in
+                detail. The project follows a structured approach with exercises
+                covering JavaScript fundamentals, DOM interaction, API usage, UI
+                enhancements, and deployment to platforms like GitHub Pages.
+                <br></br>
+                <a
+                  href="https://sanimations.github.io/pokedex-js/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Pokédex Webpage
+                </a>
+                <br></br>
+                <a
+                  href="https://github.com/sanimations/pokedex-js"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Pokédex Github Repository
+                </a>
+              </p>
             </div>
 
             {/* Project 4 */}
@@ -117,7 +189,28 @@ function App() {
               </a>
               <p></p>
               <p>
-                Created a Meet App to find interesting events around the world.
+                My objective was to build a serverless, progressive web
+                application (PWA) with React using a test-driven development
+                (TDD) technique. The application uses the Google Calendar API to
+                fetch upcoming events and used AWS Lambda functions to implement
+                serverless technology. The application was deployed on my github
+                pages.
+                <br></br>
+                <a
+                  href="https://sanimations.github.io/meet/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  MeetApp Webpage
+                </a>
+                <br></br>
+                <a
+                  href="https://github.com/sanimations/meet"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  MeetApp Github
+                </a>
               </p>
             </div>
 
@@ -138,7 +231,29 @@ function App() {
                 />
               </a>
               <p></p>
-              <p>My queer movie database created with Angular.</p>
+              <p>
+                Using Angular, I built the client-side for an app called myFlix
+                based on its existing server-side code. The server-side code
+                came from a REST API and database I had created. In this project
+                I used the MEAN tech stack. This was my first time using
+                Angular.
+                <br></br>
+                <a
+                  href="https://sanimations.github.io/myFlixAngularClient/welcome"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  MyFlix Angular Webpage
+                </a>
+                <br></br>
+                <a
+                  href="https://github.com/sanimations/myFlixAngularClient"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  MyFlix Angular Github
+                </a>
+              </p>
             </div>
 
             {/* Project 6 */}
@@ -159,8 +274,24 @@ function App() {
               </a>
               <p></p>
               <p>
-                This is a chat app I created with React Native! A mobile native
-                app with chat, image, and location sharing.
+                My objective was to build a chat app for mobile devices using
+                React Native. The app will provide users with a chat interface
+                and options to share images and their location. The data was
+                stored with Google Firestore. I used GiftedChat to send messages
+                and other data. The deployment environment was setup to work
+                with React Native and Expo.
+                <br></br>
+                {/* <a href="placeholder" target="_blank" rel="noopener noreferrer">
+                  ****Update Youtube demo
+                </a>
+                <br></br> */}
+                <a
+                  href="https://github.com/sanimations/WalkinTalkin"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  WalkinTalkin App Github
+                </a>
               </p>
             </div>
           </div>
@@ -172,26 +303,43 @@ function App() {
         <h2 style={{ color: "#66CDAA" }}>Other Work</h2>
         <p>
           I worked as a Freelance Animator for Edgeworx Studios, most notably on
-          the first season of the Hulu Show called "Wild Crime".
-          <br></br>I also worked on a music video for the band NephewCube on
-          Spotify. Here is the{" "}
+          the first season of the Hulu Show called{" "}
           <a
-            href="https://www.youtube.com/watch?v=oIYrEnYwzGk"
+            href="https://www.imdb.com/name/nm15804493/?ref_=ttfc_fc_cr"
             target="_blank"
             rel="noopener noreferrer"
           >
-            link
+            "Wild Crime"
           </a>
-          {/* Imbed the links for my youtube and possibly an IMDB page or something */}
+          .<br></br>
+          <br></br>I also worked on a music video for the band NephewCube on
+          Spotify.
         </p>
+        <div className="video-container">
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/oIYrEnYwzGk"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </div>
       </section>
 
       {/* About Me */}
       <section id="about-me">
         <h2 style={{ color: "#66CDAA" }}>About Me</h2>
         <p>
-          New Jersey born and raised. Avid gamer. Discord Server Configuration
-          Management. Easy to teach, great communicator, and responsible worker.
+          Hey there! I'm Sam Martucci, a native of New Jersey with a passion for
+          technology and gaming. With a background in Film/Electronic Arts and a
+          knack for server management, I've honed my skills in both creative and
+          technical realms. When I'm not diving into code or managing my Discord
+          server, you'll often find me immersed in the latest gaming adventures.
+          As a natural communicator and eager learner, I thrive on sharing
+          knowledge and collaborating on projects. Let's connect and explore the
+          exciting possibilities together!
         </p>
       </section>
 
